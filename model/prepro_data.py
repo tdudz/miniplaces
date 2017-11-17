Fixed Augmentation. Removed normalization of pixel values

model/prepro_data.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import h5py
 import scipy.misc
+from imgaug import augmenters as iaa
+from PIL import Image
 
 def createH5(params):
     # create output h5 file
@@ -28,11 +30,12 @@ def createH5(params):
 
     im_set = f_h5.create_dataset("images", (N,params['img_resize'],params['img_resize'],3), dtype='uint8') # space for resized images
     f_h5.create_dataset("labels", dtype='uint8', data=list_lab)
-
+    images = []
     for i in range(N):
         image = scipy.misc.imread(list_im[i])
         assert image.shape[2]==3, 'Channel size error!'
         image = scipy.misc.imresize(image, (params['img_resize'],params['img_resize']))
+
 
         im_set[i] = image
 

model/tensorflow/DataLoader.py
@@ -3,6 +3,8 @@ import numpy as np
 import scipy.misc
 import h5py
 np.random.seed(123)
+from imgaug import augmenters as iaa
+from PIL import Image
 
 # loading data from .h5
 class DataLoaderH5(object):
@@ -32,13 +34,17 @@ class DataLoaderH5(object):
         
         for i in range(batch_size):
             image = self.im_set[self._idx]
-            image = image.astype(np.float32)/255. - self.data_mean
+            print "Image #: " + str(i)
+            print image
+            #image = image.astype(np.float32)/255. - self.data_mean
             if self.randomize:
                 flip = np.random.random_integers(0, 1)
                 if flip>0:
                     image = image[:,::-1,:]
                 offset_h = np.random.random_integers(0, self.load_size-self.fine_size)
                 offset_w = np.random.random_integers(0, self.load_size-self.fine_size)
+                
+
             else:
                 offset_h = (self.load_size-self.fine_size)//2
                 offset_w = (self.load_size-self.fine_size)//2