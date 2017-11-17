4f7db4c0bd340c3e31113113e4dbe77b7e06edab
Kelly Hoffman <kellyhoffman@dhcp-18-111-78-75.dyn.mit.edu>
2017-11-17 00:46:37 -0500

    Added image augmentation using imgaug, removed randomized brightness, contrast, gamma

--- model/tensorflow/tf_train.py
+++ model/tensorflow/tf_train.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import argparse
 import tf_models
 from DataLoader import *
+from imgaug import augmenters as iaa
 
 # Command Line Argument Parsing
 parser = argparse.ArgumentParser(description='TensorFlow Model Trainer')
@@ -102,6 +103,48 @@ with tf.Session() as sess:
     while step < training_iters:
         # Load a batch of training data
         images_batch, labels_batch = loader_train.next_batch(batch_size)
+        seq = iaa.Sequential([
+            iaa.Fliplr(0.5), # horizontal flips
+            iaa.Flipud(0.5), #vertical flips
+            iaa.Sometimes(0.5,
+                iaa.Crop(percent=(0, 0.1))
+            ), # random crops
+            # Small gaussian blur with random sigma between 0 and 0.5.
+            # But we only blur about 50% of all images.
+            iaa.Sometimes(0.5,
+                iaa.GaussianBlur(sigma=(0, 0.5))
+            ),
+            # Strengthen or weaken the contrast in each image.
+            iaa.Sometimes(0.5,
+                iaa.ContrastNormalization((0.75, 1.5))
+            ),
+            # Add gaussian noise.
+            # For 50% of all images, we sample the noise once per pixel.
+            # For the other 50% of all images, we sample the noise per pixel AND
+            # channel. This can change the color (not only brightness) of the
+            # pixels.
+            iaa.Sometimes(0.5,
+                iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5)
+            ),
+            # Make some images brighter and some darker.
+            # In 20% of all cases, we sample the multiplier once per channel,
+            # which can end up changing the color of the images.
+            iaa.Sometimes(0.5,
+                iaa.Multiply((0.8, 1.2), per_channel=0.2)
+            ),
+            # Apply affine transformations to each image.
+            # Scale/zoom them, translate/move them, rotate them and shear them.
+            iaa.Sometimes(0.5,
+                iaa.Affine(
+                    scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                    translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+                    rotate=(-25, 25),
+                    shear=(-8, 8)
+                ))
+            ], random_order=True) # apply augmenters in random order
+
+        images_batch= seq.augment_images(images_batch)
+        print "Applied Augmentation"
         
         if step % step_display == 0:
             print('[%s]:' %(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
